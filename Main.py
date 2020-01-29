469eec2d85b4d15ec9e3966d53783e50b8c3282c
belopakhovart <57491175+belopakhovart@users.noreply.github.com>
2020-01-29 17:20:13 +0400

    Delete Main.py
    Удаление ненужных файлов

--- Main.py
+++
@@ -1,9 +0,0 @@
-def  human_read_format(size):
-    if size // 1024 < 1024 and size % 1024 < 512:
-        return str(size % 1024) + 'Б'
-    elif size // 1024 < 1024 and size % 1024 > 512:
-        return str(size % 1024 + 1) + 'Б'
-    elif size // 1024 < 1024 and size % 1024 > 512:
-        return str(size % 1024 + 1) + 'Б'
-
-print(human_read_format(125))
